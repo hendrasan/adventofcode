31cbd6931157f41fb9a4e6dc87555bdce2743dd5
Hendra Susanto <anotherhendrasusanto@gmail.com>
2022-12-03 16:59:12 +0700

    day3

---
+++ 2022/day3/a.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const fs = require("fs");
+const { mapLettersToNumbers } = require("./utils");
+
+const input = fs
+  .readFileSync(path.join(__dirname, "input.txt"), "utf8")
+  .split("\n");
+
+// console.log(input);
+
+const results = [];
+
+input.forEach((line, i) => {
+  let rucksack = line.trim();
+  //   console.log("rucksack", rucksack);
+  //   console.log("length", rucksack.length);
+
+  const left = rucksack.substring(0, rucksack.length / 2);
+  const right = rucksack.substring(rucksack.length / 2, rucksack.length);
+  //   console.log("left", left);
+  //   console.log("right", right);
+
+  const leftMap = new Set();
+  for (const char of left) {
+    if (!leftMap.has(char)) {
+      leftMap.add(char, true);
+    }
+  }
+
+  const rightMap = new Set();
+  for (const char of right) {
+    if (!rightMap.has(char)) {
+      rightMap.add(char, true);
+    }
+  }
+
+  for (const char of rightMap) {
+    if (leftMap.has(char)) {
+      results.push(char);
+    }
+  }
+});
+
+// console.log(results);
+
+const total = results.reduce((acc, curr) => {
+  return acc + mapLettersToNumbers(curr);
+}, 0);
+
+console.log(total);
---
+++ 2022/day3/b.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const fs = require("fs");
+const { mapLettersToNumbers } = require("./utils");
+
+const input = fs
+  .readFileSync(path.join(__dirname, "input.txt"), "utf8")
+  .split("\n");
+
+let groups = [];
+for (let i = 0; i < input.length; i += 3) {
+  groups.push(input.slice(i, i + 3));
+}
+
+const results = [];
+
+groups.forEach((elf, i) => {
+  const arrayOfSets = [];
+
+  elf.forEach((line, i) => {
+    let rucksack = line.trim();
+    // console.log("rucksack", rucksack);
+
+    const elfSet = new Set();
+    for (const char of rucksack) {
+      elfSet.add(char, true);
+    }
+
+    arrayOfSets.push(elfSet);
+  });
+
+  const intersection = arrayOfSets.reduce(
+    (a, b) => new Set([...a].filter((x) => b.has(x)))
+  );
+
+  const [value] = intersection;
+  results.push(value);
+});
+
+const total = results.reduce((acc, curr) => {
+  return acc + mapLettersToNumbers(curr);
+}, 0);
+
+console.log(total);
