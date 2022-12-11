545d745df030d531882fe3150c6ad47c0cf4fbcb
Hendra Susanto <anotherhendrasusanto@gmail.com>
2022-12-11 13:44:29 +0700

    day11

---
+++ 2022/day11/README.md
@@ -0,0 +1,7 @@
+Spent a lot of time parsing the input into processable array.
+
+For part 2, I thought I had it in the bag, but had no idea what the LCM thing is, had to look for help in AOC subreddit.
+
+In retrospect, it kinda makes sense, but without any clue from the wording, it's kinda tough to expect anyone who has never encountered this type of problem before would've thought of it.
+
+For example, instead of using the LCM, why can't we just use another magic number?
---
+++ 2022/day11/a.js
@@ -0,0 +1,151 @@
+const path = require("path");
+const fs = require("fs");
+
+const input = fs
+  .readFileSync(path.join(__dirname, "input.txt"), "utf8")
+  .split(/\n\s*\n/);
+
+let monkeys = [];
+
+input.forEach((monkey, i) => {
+  const monkeyLines = monkey.split(/\n/);
+
+  let monkeyObj = {
+    items: [],
+    increaseWorry: null,
+    divisor: null,
+    ifTrue: null,
+    ifFalse: null,
+    inspections: 0,
+  };
+
+  monkeyLines.forEach((line, j) => {
+    line = line.trim();
+
+    if (line.includes("Starting items:")) {
+      const items = line
+        .split(":")[1]
+        .trim()
+        .split(", ")
+        .map((item) => parseInt(item));
+      monkeyObj.items = items;
+    }
+
+    if (line.includes("Operation: new = ")) {
+      monkeyObj.increaseWorry = (worry) => {
+        const operation = line.split(" = ")[1].trim();
+        let [x, operand, y] = operation.split(" ");
+
+        if (y === "old") {
+          y = worry;
+        }
+
+        y = parseInt(y);
+
+        let calc = null;
+        if (operand === "*") {
+          calc = worry * y;
+        } else if (operand === "+") {
+          calc = worry + y;
+        } else if (operand === "-") {
+          calc = worry - y;
+        }
+        return calc;
+      };
+    }
+
+    if (line.includes("Test: divisible by ")) {
+      const divBy = parseInt(line.split("by ")[1].trim());
+      monkeyObj.divisor = divBy;
+    }
+
+    if (line.includes("If true: throw to monkey ")) {
+      const destMonkey = parseInt(
+        line.split("If true: throw to monkey ")[1].trim()
+      );
+      monkeyObj.ifTrue = destMonkey;
+    }
+
+    if (line.includes("If false: throw to monkey ")) {
+      const destMonkey = parseInt(
+        line.split("If false: throw to monkey ")[1].trim()
+      );
+      monkeyObj.ifFalse = destMonkey;
+    }
+  });
+
+  monkeys.push(monkeyObj);
+});
+
+// console.log(monkeys);
+function processRound() {
+  monkeys.forEach((monkey, i) => {
+    if (monkey.items.length === 0) {
+      return;
+    }
+
+    // console.log("===Monkey ", i);
+
+    // monkey.items.forEach((item) => {
+    while (monkey.items.length) {
+      // inspect
+      monkey.inspections += 1;
+
+      // increaseWorry
+      let worry = monkey.items.shift();
+      // console.log("Monkey inspects an item with a worry level of ", worry);
+
+      worry = monkey.increaseWorry(worry);
+      // console.log("  Worry level increases to ", worry);
+
+      // monkey gets bored, divide by 3 and round down
+      worry = Math.floor(worry / 3);
+      // console.log(
+      //   "  Monkey gets bored with item. Worry level is divided by 3 to ",
+      //   worry
+      // );
+
+      // check with test case
+      let monkeyToThrow =
+        worry % monkey.divisor === 0 ? monkey.ifTrue : monkey.ifFalse;
+      // console.log(
+      //   "  Check if worry level is divisible by ",
+      //   monkey.divisor,
+      //   worry % monkey.divisor === 0
+      // );
+
+      // throw to other monkey
+      // console.log(
+      //   "  Item with worry level",
+      //   worry,
+      //   "thrown to monkey",
+      //   monkeyToThrow
+      // );
+      monkeys[monkeyToThrow].items.push(worry);
+    }
+    // });
+  });
+}
+
+for (let i = 1; i <= 20; i++) {
+  processRound();
+  // console.log("After Round", i);
+  // monkeys.map((monkey, i) => {
+  //   console.log(
+  //     "Monkey",
+  //     i,
+  //     ":",
+  //     monkey.items,
+  //     "| Inspections",
+  //     monkey.inspections
+  //   );
+  // });
+}
+
+const topTwo = monkeys
+  .sort((a, b) => b.inspections - a.inspections)
+  .slice(0, 2);
+
+console.log(topTwo.reduce((acc, cur) => acc * cur.inspections, 1));
+
+// console.log(monkeys);
